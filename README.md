433768fdaba18ed76573aee5d9d0512d84013be1
Mx. Wolfe <mx_wolf_6221@protonmail.com>
2021-10-24 13:53:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Happy Birthday
+
+This program checks today's date against the `date` string variable imported from `mybday.py`. 
+
+Then, if the variables match  *(eg. "Nov05")*, then a birthday message will be generated with the lyrics from The Beatles' song, *'Birthday'* 
+and post it to the Twitter account linked to the API credentials stored in `creds.py`.
+
+**Please make sure to create both `mybday.py` and `creds.py` *BEFORE* running the program. Failure to do so will cause errors.**
+
+
+## `mybday.py` Example
+```
+date = "Nov05"
+
+```
+
+
+## `creds.py` Example
+```
+consumer_key = 'API-KEY'
+consumer_secret = 'API-SECRET'
+access_token = 'API-ACCESS-TOKEN'
+access_token_secret = 'API-ACCESS-TOKEN-SECRET'
+
+```
+
+### If you have any questions, please feel free to ask. Have a great (birth)day! ;-)
---
+++ main.py
@@ -0,0 +1,71 @@
+import datetime
+import time
+import tweepy
+import creds
+import mybday
+
+# OAuth Handler
+auth = tweepy.OAuthHandler(
+    creds.consumer_key,
+    creds.consumer_secret
+)
+auth.set_access_token(
+    creds.access_token,
+    creds.access_token_secret
+)
+api = tweepy.API(
+    auth,
+    wait_on_rate_limit=True
+)
+
+
+# Rate Limit Handler
+def limit_handled(cursor):
+    while True:
+        try:
+            yield next(cursor)
+        except tweepy.TooManyRequests:
+            time.sleep(15 * 60)
+
+
+# Verify Twitter API Credentials #
+def verify_creds():
+    try:
+        api.verify_credentials()
+        print('Authentication OK')
+    except tweepy.errors.TweepyException:
+        print('Error during authentication')
+
+
+# Date Variables
+class myBirthday:
+    def __init__(self):
+        # Date object variables
+        self.today = datetime.date.today().strftime("%b" + "%d")
+        self.birthday = mybday.date
+
+
+bday = myBirthday()
+
+
+# Compare today's date with birthday variable
+def check_date():
+    try:
+        birthday = bday.today == bday.birthday
+        return birthday
+    except:
+        print('Program encountered an unexpected error')
+
+
+def post_msg():
+    if check_date():
+        print('Posting your birthday message to Twitter...\n')
+        with open('lyrics.txt', 'r') as lyrics:
+            api.update_status("#Day8 of #100DaysOfCode... If I coded this correctly, it should post a celebratory "
+                              "message just for me!\n\n" + lyrics.read())
+        print('Your Twitter status has been successfully updated...\n\tHappy Birthday!')
+    else:
+        print("It's not your birthday yet, dweeb!\n\tCheck back another time.")
+
+
+post_msg()
